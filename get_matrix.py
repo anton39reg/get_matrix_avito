311143d6783fc0adb3b041f239cda503aadb8c69
Pilkevich Anton <pilkevich.aa@phystech.edu>
2021-09-09 15:19:33 +0300

    add try except

--- get_matrix.py
+++ get_matrix.py
@@ -10,6 +10,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def spiral_order(matrix: List[List[int]]) -> List[int]:
+    # source: https://leetcode.com/problems/spiral-matrix/solution/
+
     result = []
     rows, columns = len(matrix), len(matrix[0])
     up = left = 0
@@ -17,23 +19,17 @@ def spiral_order(matrix: List[List[int]]) -> List[int]:
     down = rows - 1
 
     while len(result) < rows * columns:
-        # Traverse from left to right.
         for row in range(up, down + 1):
             result.append(matrix[row][left])
 
-        # Traverse downwards.
         for col in range(left + 1, right + 1):
             result.append(matrix[down][col])
 
-        # Make sure we are now on a different row.
         if left != right:
-            # Traverse from right to left.
             for row in range(down - 1, up - 1, -1):
                 result.append(matrix[row][right])
 
-        # Make sure we are now on a different column.
         if up != down:
-            # Traverse upwards.
             for col in range(right - 1, left, -1):
                 result.append(matrix[up][col])
 
@@ -46,11 +42,20 @@ def spiral_order(matrix: List[List[int]]) -> List[int]:
 
 
 async def get_matrix(url: str) -> List[int]:
+
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            logging.info(resp.status)
-            data = await resp.text()
-            logging.info(data)
+        try:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    logging.info(f'Status: {resp.status}')
+                    data = await resp.text()
+                    logging.info(f'Data: {data}')
+                else:
+                    logging.errir(f'Status: {resp.status}')
+                    return None
+        except aiohttp.ClientConnectorError as e:
+            logging.error(f'Connection Error: {str(e)}')
+            return None
 
     tmp = ''.join(data.strip().split('\n')[1::2]).replace(' ', '').split('||')
     logging.info(tmp)
@@ -58,8 +63,7 @@ async def get_matrix(url: str) -> List[int]:
     matrix = list(map(lambda x: list(map(int, filter(len, x.split('|')))), tmp))
     logging.info(matrix)
 
-    loop = asyncio.get_event_loop()
-    result = await loop.run_in_executor(None, spiral_order, matrix)
+    result = spiral_order(matrix)
     logging.info(result)
 
     return result
